d781dc31871e8371f24ba6199ed937d78db5459f
Rishabh gupta <rishabh@innovatia.co.in>
2023-03-30 13:20:22 +0530

    error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Error = () => {
-  return <div>Error</div>;
+  return (
+    <div className="h-screen">
+      <div className="text-9xl flex flex-col translate-y-full items-center">
+        <h1 className="font-semibold text-cyan-500">404 Error</h1>
+        <p className="text-6xl text-cyan-600">Page Not Found!</p>
+      </div>
+    </div>
+  );
 };
 
 export default Error;
